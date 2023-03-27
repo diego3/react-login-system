65ec8da76a37af103ce67960b92d5c96ee60c02e
diego.santos <www.diegosantos.com.br@gmail.com>
2023-03-27 19:39:39 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [message, setMessage] = useState();
+
+  function handleAcessar() {
+    if (email === "diego@gmail.com" && password === "123456") {
+      setMessage("Acessado com sucesso");
+    } else {
+      setMessage("Usuário ou senha incorretos");
+    }
+  }
+
+  function handleInput(e) {
+    const name = e.target.name
+    const value = e.target.value
+    if (name === "email") {
+      setEmail(value)
+    } else {
+      setPassword(value)
+    }
+  }
+
+  function handleInputFocus() {
+    setMessage("")
+  }
+
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      handleAcessar()
+    }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="header">
+        <div className="container">
+          <img src={logo} className="react-logo" alt="logo" />
+          <p>Login</p>
+
+          <input type="text" name="email" value={email} title="email"
+              placeholder="Digite seu email" 
+              onChange={handleInput}
+              onFocus={handleInputFocus}></input>
+          
+          <input type="text" name="password" value={password} title="password"
+              placeholder="Digite sua senha"  
+              onChange={handleInput}
+              onFocus={handleInputFocus}></input>
+
+          <button
+            className="button"
+            onClick={(e) => handleAcessar(e)}
+          >
+            Acessar
+          </button>
+          <p>
+            <span title="message"></span>{message}
+          </p>
+        </div>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('deve renderizar todos os componentes principais da página de login', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/acessar/i);
+  const inputEmail = screen.getByTitle("email");
+  const inputPassword = screen.getByTitle("password");
+
   expect(linkElement).toBeInTheDocument();
+  expect(inputEmail).toBeInTheDocument();
+  expect(inputPassword).toBeInTheDocument();
 });
